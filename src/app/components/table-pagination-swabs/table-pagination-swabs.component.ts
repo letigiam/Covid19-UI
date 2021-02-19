0114af14bb1253e5fdd6fbf89ad7bab496458a81
mirkotorrisi <mirko.torrisi92@gmail.com>
2021-02-19 12:19:49 +0100

    changed some graphics

--- src/app/components/table-pagination-swabs/table-pagination-swabs.component.ts
+++ src/app/components/table-pagination-swabs/table-pagination-swabs.component.ts
@@ -1,9 +1,10 @@
 import { Router } from '@angular/router';
 import { Component, Input, OnInit } from '@angular/core';
-import { Swab, SwabCalendar } from 'src/app/interface/list-of-swabs';
 import { SwabsService } from 'src/app/services/swabs.service';
 import * as moment from 'moment';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Swab } from 'src/app/interface/list-of-swabs';
 
 @Component({
   selector: 'app-table-pagination-swabs',
@@ -25,7 +26,13 @@ export class TablePaginationSwabsComponent implements OnInit {
   endDate!: Date;
 
   time = { hour: 13, minute: 30 };
-  constructor(private swabsService: SwabsService, private router: Router) {}
+  editProfileForm: any;
+  constructor(
+    private swabsService: SwabsService,
+    private router: Router,
+    private modalService: NgbModal,
+    private fb: FormBuilder
+  ) {}
 
   async ngOnInit() {
     try {
@@ -41,6 +48,12 @@ export class TablePaginationSwabsComponent implements OnInit {
       (i) => moment(i).format('dddd') + ' ' + moment(i).format('DD-MM')
     );
     this.daysSelectedContent = Object.values(this.allSwabs);
+    this.editProfileForm = this.fb.group({
+      address: [''],
+      email: [''],
+      phone: [''],
+      hasCovid: [''],
+    });
   }
   getSwabsByDate = async () => {
     this.allSwabs = await this.swabsService.allSwabsByDate(
@@ -52,4 +65,66 @@ export class TablePaginationSwabsComponent implements OnInit {
     );
     this.daysSelectedContent = Object.values(this.allSwabs);
   };
+
+  // openModal(targetModal: any, swab: Swab) {
+  //   this.modalService.open(targetModal, {
+  //     centered: true,
+  //     backdrop: 'static',
+  //   });
+
+  //   this.editProfileForm.patchValue({
+  //     patient,
+  //     email: patient.email,
+  //     phone: patient.phone,
+  //     hasCovid: patient.hasCovid,
+  //   });
+  // }
+
+  // async onSubmit() {
+  //   this.modalService.dismissAll();
+  //   this.patientModified = this.editProfileForm.getRawValue();
+  //   this.putPatient();
+  // }
+  // async onDelete() {
+  //   this.delPatient();
+  //   this.modalService.dismissAll();
+  //   this.patients = await this.patientsService.getAllPatients();
+  // }
+  // putPatient = () => {
+  //   this.patientsService
+  //     .putPatient(
+  //       this.id_patient,
+  //       this.patientModified.address,
+  //       this.patientModified.email,
+  //       this.patientModified.phone,
+  //       Number(this.patientModified.hasCovid)
+  //     )
+  //     .subscribe(
+  //       async (Response) => {
+  //         this.message = 'Patient Modified';
+  //         this.patients = await this.patientsService.getAllPatients();
+  //       },
+  //       (error) => {
+  //         this.message = 'Error';
+  //         console.log('Error is, ', error);
+  //       }
+  //     );
+  // };
+
+  // setId(patient_id: string) {
+  //   console.log(patient_id);
+  //   this.id_patient = patient_id;
+  // }
+
+  // delSwab = () => {
+  //   this.patientsService.deletePatient(this.id_patient).subscribe(
+  //     (Response) => {
+  //       this.message = 'Patient Deleted';
+  //     },
+  //     (error) => {
+  //       this.message = 'Error';
+  //       console.log('Error is, ', error);
+  //     }
+  //   );
+  // };
 }
